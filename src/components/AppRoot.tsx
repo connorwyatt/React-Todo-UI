96576e0a179da7f3220830d237ca94738b17f8c5
Connor Wyatt <connorwyatt1@gmail.com>
2017-04-30 13:05:33 +0100

    feat(todo list): add todo list component

--- src/components/AppRoot.tsx
+++ src/components/AppRoot.tsx
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './AppRoot.scss';
+import {TodoList} from './TodoList';
 
 export class AppRoot extends Component<void, void> {
   public render() {
     return <div className="AppRoot">
-      <h1 className="AppRoot-title">React App</h1>
-      <img className="AppRoot-image" src="/assets/images/react-logo.svg"/>
+      <h1 className="AppRoot-title">React Todo UI</h1>
+
+      <TodoList/>
     </div>;
   }
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,5 @@
+import React, {StatelessComponent} from 'react';
+
+export const TodoList: StatelessComponent<void> = () => {
+  return <div>Todo List</div>;
+};
